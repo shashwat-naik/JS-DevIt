1c566468f0ab59983677cf4f2171dd18bb7fce22
Shashwat Naik <naikshashwat3@gmail.com>
2025-07-22 13:20:21 +0530

    22 July Day Nine

---
+++ DayNine_22July/DayNine_22July.js
@@ -0,0 +1,61 @@
+// Q1 Write a loop from 1 to 100 that skips the multiple of 3 and 5
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 || i % 5 == 0) continue;
+  console.log(i);
+}
+
+// Q2 Given an input element with type="email", fetch the value of the type attribute using JavaScript
+// let inp = document.getElementById("inp");
+// var btn = document.getElementById("submit");
+
+// btn.addEventListener("click", function () {
+//   let change = inp.getAttribute("type");
+//   let ans = inp.value;
+//   console.log(change);
+//   console.log(`value inside it is ${ans}`);
+// });
+
+// Q3 Write a function to hide a paragraph when a button is clicked.
+// let para = document.getElementById("content");
+// var btn = document.getElementById("magic");
+// let alternative = 1;
+// btn.addEventListener("click", function () {
+//   if (alternative == 1) {
+//     para.style.display = "none";
+//     alternative = 0;
+//   } else {
+//     alternative = 1;
+//     para.style.display = "block";
+//   }
+// });
+
+// Q4 Prompt the user to enter their age and display it in an alert.
+let value = prompt("Enter your age: ");
+console.log(value);
+
+// Q5 Create a form and reset it using a button.
+
+// var btn=document.getElementById("click");
+// var type=document.getElementById("details");
+// btn.addEventListener("click", function(){
+//   type.reset()
+// })
+
+// function myFun() {
+//   document.getElementById("details").reset();
+// }
+
+// Q6 Write a function that returns the square of a number.
+function result(value) {
+  console.log(`Square of ${value} is ${value * value}.`);
+}
+result(prompt("Enter a value: "));
+
+// Q7 Split the String "red,green,blue" into an array.
+let a = "red,green,blue";
+const arr = a.split(",");
+console.log(arr);
+
+// Q8 Use a ternary operator to display "Pass" if marks are above 40, otherwise "Fail".
+var marks = prompt("Enter your mark out of 100: ");
+console.log(marks > 40 ? "Pass" : "Fail");
